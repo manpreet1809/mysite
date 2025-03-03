4634da9bf30d6deb53bb268b5adc6f237a479861
manpreetkaur <kalsi828@gmail.com>
2025-03-03 12:48:10 -0500

    Extend core title component

--- core/src/main/java/com/mysite/core/models/CustomTitle.java
+++ core/src/main/java/com/mysite/core/models/CustomTitle.java
@@ -1,12 +1,39 @@
 package com.mysite.core.models;
 
 
-public interface CustomTitle {
-    /**
-     * Retrieves the title of the custom model.
-     *
-     * @return The title of the custom model.
-     */
-     String getTitle();
+import com.adobe.cq.wcm.core.components.commons.link.Link;
+import com.adobe.cq.wcm.core.components.models.Component;
+import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
+
+import com.drew.lang.annotations.NotNull;
+import com.drew.lang.annotations.Nullable;
+
+
+public interface CustomTitle extends Component {
+     String getText();
+
+     String getType();
+     @Nullable
+     Link getLink();
+
+     @Deprecated
+     String getLinkURL();
+
+     boolean isLinkDisabled();
+
+     @NotNull
+     String getId();
+
+     @NotNull
+     ComponentData getData();
+
+     @NotNull
+     String getAppliedCssClasses();
+
+     @NotNull
+     String getExportedType();
+
+     String getSubTitle();
+
 
 }
